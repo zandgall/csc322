1239fc5a4d4c728263ce5753cd853e08bcc0abe7
Zandgall <Alexanderdgall@gmail.com>
2024-06-05 16:00:38 -0500

    Session 5 Assignment WIP

--- com/zandgall/csc322/session5/assignment/AddressBook.java
+++ com/zandgall/csc322/session5/assignment/AddressBook.java
@@ -11,6 +11,10 @@
 package com.zandgall.csc322.session5.assignment;
 
 import java.util.ArrayList;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -31,19 +35,22 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 import javafx.collections.ObservableList;
+import javafx.beans.value.ObservableValue;
+import javafx.beans.value.ChangeListener;
 import javafx.collections.FXCollections;
 
 public class AddressBook extends Application {
 
 	// Data
 	ArrayList<Entry> entries = new ArrayList<Entry>();
-	Entry currentEntry = null;
+	int currentSelection = 0;
 
 	/* UI elements */
 
 	Label title = new Label("Address Book");
 	ListView<String> list;
-	ObservableList<String> listContent = FXCollections.observableArrayList();	
+	ObservableList<String> listContent = FXCollections.observableArrayList();
+	ChangeListener<? super Number> listListener;
 
 	// Form content
 	GridPane content = new GridPane(10, 10);
@@ -74,8 +81,7 @@ public class AddressBook extends Application {
 
 		BorderPane mainPane = new BorderPane();
 
-		// Add list and title
-		listContent.addAll("a", "b", "c");
+		// Add list and title	
 		list = new ListView<String>(listContent);
 		mainPane.setMargin(list, new Insets(12,12,12,12));
 		mainPane.setLeft(list);
@@ -83,11 +89,20 @@ public class AddressBook extends Application {
 		mainPane.setTop(title);
 		mainPane.setAlignment(title, Pos.CENTER);
 
+		listListener = new ChangeListener<Number>() {
+			@Override
+			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+				switchFromTo(oldValue.intValue(), newValue.intValue());
+			}
+		};
+		list.getSelectionModel().selectedIndexProperty().addListener(listListener);
+
 		// Add buttons
 		buttonContainer.getChildren().addAll(add, delete, update);
 		buttonContainer.setAlignment(Pos.CENTER);
 		mainPane.setMargin(buttonContainer, new Insets(12, 12, 12, 12));
 		mainPane.setBottom(buttonContainer);
+		add.setOnAction(e -> {addEntryFromForm();});
 
 		// Add form content
 		content.setAlignment(Pos.CENTER);	
@@ -115,24 +130,83 @@ public class AddressBook extends Application {
 		stage.setScene(scene);
 		stage.show();
 
+		autoload();
+	}
 
+	private void switchFromTo(int previous, int current) {
+		// Don't update entry if none were selected last
+		if(previous != -1) {
+			updateEntryFromForm(entries.get(previous));
+			listContent.set(previous, firstNameField.getText() + " " + lastNameField.getText());
+		}
+		// Don't update form if no entry is selected now, or selected out of bounds
+		if(current < 0 || current >= entries.size())
+			updateFormFromEntry(entries.get(current));
+		autosave();
+		currentSelection = current;
+	}
+
+	private void updateEntryFromForm(Entry e) {
+		firstNameField.setText(e.firstName);
+		lastNameField.setText(e.lastName);
+		streetField.setText(e.street);
+		cityField.setText(e.city);
+		phoneField.setText(e.phone);
+		emailField.setText(e.email);
+		notesField.setText(e.notes);
+		// TODO: State
+	}
+
+	private void updateFormFromEntry(Entry e) {
+		e.firstName = firstNameField.getText();
+		e.lastName = lastNameField.getText();
+		e.street = streetField.getText();
+		e.city = cityField.getText();
+		e.phone = phoneField.getText();
+		e.email = emailField.getText();
+		e.notes = notesField.getText();
+		// TODO: State
 	}
 
 	private void autoload() {
-		
+		try { 
+			ObjectInputStream s = new ObjectInputStream(new FileInputStream("data.bin"));
+			while(s.available()>0)
+				entries.add((Entry)s.readObject());
+			// If there is a selected entry, update the form to reflect
+			if(currentSelection != -1)
+				updateFormFromEntry(entries.get(currentSelection));
+		} catch(Exception e) {
+			System.out.println("Could not open database, using blank template");
+		}
+	}
+
+	private void addEntryFromForm() {
+		currentSelection = entries.size();
+		entries.add(new Entry());
+		listContent.add(firstNameField.getText() + " " + lastNameField.getText());
+		updateEntryFromForm(entries.get(currentSelection));
 	}
 
 	private void autosave() {
+		// Make sure current entry is updated (if one is selected) before writing it
+		if(currentSelection == -1)
+			updateEntryFromForm(entries.get(currentSelection));
 
+		try {
+			ObjectOutputStream s = new ObjectOutputStream(new FileOutputStream("data.bin"));
+			for(Entry e : entries)
+				s.writeObject(e);
+		} catch(Exception e) {
+			System.err.println("Could not save database!");
+			e.printStackTrace();
+		}
 	}
 
-	private void updateListContent() {
-
-	}
-
+	// A simple storage class
 	private static class Entry {
-		String firstName, lastName, street, city, phone, email, notes;
-
+		public String firstName = "", lastName = "", street = "", city = "", phone = "", email = "", notes = "";
+		public Entry() {}
 	}
 }
 
